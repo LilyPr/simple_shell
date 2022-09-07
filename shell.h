836ae30125de9985cc7fb3b8fdeb61b657449954
Lily <106800670+LilyPr@users.noreply.github.com>
2022-09-07 15:47:34 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -27,39 +27,39 @@ int main(int argc, char **argv);
 
 /* input */
 void rock(char *coral);
-char *read_line(void);
-char **split_line(char *buffer, char *del);
-int execute_shell(char **args, char *snail);
+char *_readline(void);
+char **bridge_line(char *buffer, char *del);
+int _executes(char **args, char *colar);
 
 /* path functions */
-path_t *path_gen(void);
-char *path_check(path_t *head, char *input);
-char *path_cat(char *s1, char *s2);
+path_t *generate_pth(void);
+char *check_path(path_t *head, char *input);
+char *path_conc(char *s1, char *s2);
 
 /* built in functions*/
-int isit_built(char **args, char *input);
+int built_func(char **args, char *input);
 void shell_exit(char **args, char *input);
 void print_enviornment(void);
 
 /* enviornment funcitons */
 path_t *_env(void);
-char *env_find(char *var);
-int env_match(char *input);
-void env_start(char *str, char *value);
+char *find_enve(char *var);
+int match_enve(char *input);
+void _enve(char *str, char *value);
 
 /* string tokenize functions*/
-int ayto(char z, const char *delim);
-char *_strtok(char *input, const char *delim);
-char **split_line(char *buffer, char *delim);
-int word_count(char *str);
+int pass(char k, const char *delimeter);
+char *_strtok(char *input, const char *delimeter);
+char **bridge_line(char *buffer, char *delimeter);
+int count_wrd(char *str);
 
 /* execution */
-int cmp_exec(char **args, char *eggs);
-int fork_exec(char **args, char *snail);
+int _exec(char **args, char *rock);
+int fork_exec(char **args, char *colar);
 
 /* functions that free */
-void pth_free(path_t *head);
-void env_free(path_t *head);
+void free_path(path_t *head);
+void free_enve(path_t *head);
 
 /* string functions */
 int _strlen(char *s);
@@ -69,16 +69,16 @@ char *_strdup(char *str);
 char *_strcpy(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 char *_itoa(int num);
-int _numlen(int n);
-char *_strcpy_src(char *dest, char *src, int n);
+int _numberlen(int n);
+char *_strcpysource(char *dest, char *src, int n);
 void _puts(char *str);
 /* variables */
-path_t *the_path;
-path_t *enviornment;
+path_t *_paths;
+path_t *environment;
 int line_count;
 void *_realloc(void *ptr, unsigned int before, unsigned int after);
 void errenous(char **args, char *cmd, int errno);
-void sig_handler(int sig_handler);
+void sign_handle(int sign_handle);
 /*ssize_t getline(char **lineptr, size_t *n, FILE *stream);*/
 int find_match(const char *s1, char *s2);
 char *_getenv(const char *name);
